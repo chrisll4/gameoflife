18581fa96887d24be96244132d73557c96c9a89f
TiffMo <s236600@stud.hioa.no>
2016-01-14 11:52:15 +0100

    test

--- src/gameoflife/GameofLife.java
+++ src/gameoflife/GameofLife.java
@@ -3,9 +3,11 @@ package gameoflife;
 public class GameofLife {
 
    static int yes = 1;
+   static int test = 2;
    
    public static void main(String[] args) {
       System.out.println(yes);
+      System.out.println(test);
    }
    
 }
