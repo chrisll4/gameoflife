162e330a775227b47b0c4363eccdcfccedf076fa
Christine <Christine@10.253.30.192>
2016-01-15 12:07:05 +0100

    Heiiii

--- src/gameoflife/GameofLife.java
+++ src/gameoflife/GameofLife.java
@@ -4,10 +4,12 @@ public class GameofLife {
 
    static int yes = 1;
    static int test = 2;
+   static int no = 3;
    
    public static void main(String[] args) {
       System.out.println(yes);
       System.out.println(test);
+      System.out.println(no);
    }
    
 }
